5452f5c694736c20b5d7e87739ddf7624b9e7f78
klaus19 <tejaskhartude@gmail.com>
2023-01-23 14:19:47 +0530

    added a new Neetcode 150 problems

---
+++ Arrays&Hashing/ProductArrayExceptSelf.py
@@ -0,0 +1,27 @@
+class Solution(object):
+
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n = len(nums)
+        left_product = [1] * n
+        right_product = [1] * n
+        output = [1] * n
+
+        for i in range(1, n):
+            left_product[i] = left_product[i - 1] * nums[i - 1]
+
+        for i in range(n - 2, -1, -1):
+            right_product[i] = right_product[i + 1] * nums[i + 1]
+
+        for i in range(n):
+            output[i] = left_product[i] * right_product[i]
+
+        return output
+
+
+if __name__ == '__main__':
+    pt = [1, 2, 3, 4]
+    print(Solution().productExceptSelf(pt))
